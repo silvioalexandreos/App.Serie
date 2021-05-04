63d609eebd59ea735177f124178b84aa31595b2e
Silvio Alexandre <silvio.alexandreos@gmail.com>
2021-05-04 08:28:08 -0300

    Projeto Inicial

---
+++ SAOS.Series/Entidade/EntidadeBase.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SAOS.Series
+{
+    public abstract class EntidadeBase
+    {
+        public int Id { get; protected set; }
+        public bool Excluido { get; set; }
+    }
+}
---
+++ SAOS.Series/Repositorio/SerieRepositorio.cs
@@ -0,0 +1,42 @@
+﻿using SAOS.Series.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SAOS.Series
+{
+    public class SerieRepositorio : IRepositorio<Serie>
+    {
+        private List<Serie> listaSerie = new List<Serie>();
+
+        public void Atualiza(int id, Serie objeto)
+        {
+            listaSerie[id] = objeto;
+        }
+
+        public void Exclui(int id)
+        {
+            listaSerie[id].Excluir();
+        }
+
+        public void Insere(Serie objeto)
+        {
+            listaSerie.Add(objeto);
+        }
+
+        public List<Serie> Lista()
+        {
+            return listaSerie;
+        }
+
+        public int ProximoId()
+        {
+            return listaSerie.Count;
+        }
+
+        public Serie RetornaPorId(int id)
+        {
+            return listaSerie[id];
+        }
+    }
+}
